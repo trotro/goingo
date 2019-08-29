7cd72551d0628fe1c0d33ce78028ea3fc4189667
trotro <ntrauwaen@gmail.com>
2019-08-29 09:56:17 +0000

    Changed structs & constants to export them outside of board package

--- board/board.go
+++ board/board.go
@@ -5,29 +5,30 @@ import (
 	"math"
 )
 
-const moku = '+'
-const white = 'w'
-const black = 'b'
+//Moku is the rune to represent an empty moku on the go board
+const Moku = '+'
+
+//Black is the rune to represent black stones
+const Black = 'b'
+
+//White is the rune to represent white stones
+const White = 'w'
+
+//unexported constants
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const maxSize = 19
 
 //Coord is a structure for coordinates on the go board.
 type Coord struct {
-	col rune //column letter
-	lin int  //line number
+	Col rune //column letter
+	Lin int  //line number
 }
 
 //Move is a structure to store moves data : coordinates and player.
 type Move struct {
-	player rune //player color
-	loc    Coord
+	Player rune //player color
+	Loc    Coord
 }
-
-//play registers a move on the goban
-/*func (m *Move) play(goban [][]rune) [][]rune {
-	goban[m.loc.lat][m.loc.lon] = m.player
-	return goban
-}*/
 
 //GoBoard is a map to represent the go board.
 type GoBoard map[Coord]rune
@@ -43,9 +44,14 @@ func (goban GoBoard) New(size int) {
 			break
 		}
 		for li := 1; li < size+1; li++ {
-			goban[Coord{le, li}] = moku
+			goban[Coord{le, li}] = Moku
 		}
 	}
+}
+
+//Play registers a move on the goban
+func (goban GoBoard) Play(m Move) {
+	goban[Coord{m.Loc.Col, m.Loc.Lin}] = m.Player
 }
 
 //Print prints the board on stdout.
